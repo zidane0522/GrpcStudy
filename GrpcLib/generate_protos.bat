10f65384b9553c5a73469515ca853904bce8ecd7
zidane0522 <lishen1990@163.com>
2017-03-23 11:41:43 +0800

    f

---
+++ GrpcLib/generate_protos.bat
@@ -0,0 +1,10 @@
+﻿setlocal
+
+@rem enter this directory
+cd /d %~dp0
+
+set TOOLS_PATH=packages\Grpc.Tools.1.2.0\tools\windows_x86
+
+%TOOLS_PATH%\protoc.exe -I../../protos --csharp_out GrpcLib  ../../protos/helloworld.proto --grpc_out GrpcLib --plugin=protoc-gen-grpc=%TOOLS_PATH%\grpc_csharp_plugin.exe
+
+endlocal
