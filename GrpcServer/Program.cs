ca28e07e4cacaaf47b2b5801d4267a43c616c63b
zidane0522 <lishen1990@163.com>
2017-03-23 10:52:03 +0800

    d

---
+++ GrpcServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
