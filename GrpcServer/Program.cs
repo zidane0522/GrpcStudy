36f6d108eb7186aeda45bcb1769f02d3d6754e1c
zidane0522 <lishen1990@163.com>
2017-03-23 15:46:30 +0800

    a

--- GrpcServer/Program.cs
+++ GrpcServer/Program.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grpc.Core;
+using HelloWorld;
+using System.Diagnostics;
 
 namespace GrpcServer
 {
     class Program
     {
+        const int port = 50051;
+       
+
         static void Main(string[] args)
         {
+            Server server = new Server
+            {
+                Services = { HelloWorld.GrpcLib.BindService(new Myserver()) },
+                Ports = { new ServerPort("127.0.0.1",port,ServerCredentials.Insecure) }
+            };
+            server.Start();
+            Console.ReadKey();
+            server.ShutdownAsync().Wait();
+        }
+    }
+
+    class Myserver : HelloWorld.GrpcLib.GrpcLibBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+        }
+
+        public override async Task SayHelloAgain(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            await responseStream.WriteAsync(new HelloReply { Message = "Hello " + request.Name });
+        }
+
+        public override async Task<HelloReply> SayHelloThree(IAsyncStreamReader<HelloRequest> requestStream, ServerCallContext context)
+        {
+            var stopwatch = new Stopwatch();
+            string name = "";
+            stopwatch.Start();
+           
+            while (await requestStream.MoveNext())
+            {
+                //get client msg
+                var request = requestStream.Current;
+                name = request.Name;
+            }
+            stopwatch.Stop();
+            return new HelloReply { Message = "Hello " + name };
+        }
+
+        public override Task SayHelloFour(IAsyncStreamReader<HelloRequest> requestStream, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            return base.SayHelloFour(requestStream, responseStream, context);
         }
     }
 }
--- grpc/Program.cs
+++ grpc/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,16 @@ namespace grpc
     {
         static void Main(string[] args)
         {
+            Channel channel = new Channel("127.0.0.1:50051",ChannelCredentials.Insecure);
+            var client =new HelloWorld.GrpcLib.GrpcLibClient(channel);
+            //Code goes here
+            HelloWorld.HelloReply rply= client.SayHello(new HelloWorld.HelloRequest { Name = "Zidane0522" });
+            AsyncUnaryCall<HelloWorld.HelloReply> rlly= client.SayHelloAsync(new HelloWorld.HelloRequest { Name = "Zidane0522" });
+            Console.WriteLine(rply.Message);
+            Console.WriteLine("From async"+ rlly.ResponseAsync.Result.Message); 
+            Console.ReadKey();
+            //await client.SayHelloAsync(new HelloWorld.HelloRequest { Name = "Zidane0522" });
+            channel.ShutdownAsync().Wait();
         }
     }
 }
